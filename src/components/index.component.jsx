e66622f80f914c5474f8eeae0a7116f186bd32b6
donnc <donnchao@outlook.com>
2019-08-20 22:16:09 +0800

    add react hello world

---
+++ src/components/index.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class IndexComponent extends React.Component{
+    render(){
+        return <h1>hello world!!!</h1>
+    }
+}
+
+
+var oBox = document.getElementById("box");
+ReactDOM.render(<IndexComponent/>, oBox);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-import './assets/demo.css';
-import './assets/demo2.less';
 import './assets/scss/app.scss'
 
 //添加promise profile, 支持浏览器异步加载js
@@ -9,20 +7,4 @@ import 'es6-promise/auto';
 //分开引入
 import "./bootstrap-demo/bootstrap.include.js"
 
-
-var body = document.getElementsByTagName('body')[0];
- body.innerHTML = template;
-
- //直接操作dom示例
- // var app = document.getElementById("app");
- // app.innerHTML = '<div>hello webpack</div>';
-
-
- //异步加载js示例
- import('./sync.js').then(data => {
-     console.log(data.default);
- });
-
- var a = new Promise(function (resolve,reject) {
-
- });
+import "./components/index.component.jsx"
