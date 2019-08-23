1c59a19a7255277413fce949f5e2508138bf593f
donnc <donnchao@outlook.com>
2019-08-24 01:53:39 +0800

    publish 1.0.5

--- src/components/index.component.tsx
+++ src/components/index.component.tsx
@@ -1,21 +1,21 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-// interface State {
-//     msg: string
-// }
+//import * as ReactDOM from 'react-dom';
+interface State {
+    msg: string
+}
 
-// export class IndexComponent extends React.Component<Object,State> {
-//     render(){
-//         this.state = {
-//             msg: 'hello'
-//         };
-//         return <h1>{this.state.msg} world!!!</h1>
-//     }
-// }
+export class IndexComponent extends React.Component<Object,State> {
+    render(){
+        this.state = {
+            msg: 'hello'
+        };
+        return <h1>{this.state.msg} world!!!</h1>
+    }
+}
 
-import IndexComponent from '../../dist/static/js/app';
+// import IndexComponent from '../../dist/static/js/app';
 
-console.log(IndexComponent);
+// console.log(IndexComponent);
 
-var oBox = document.getElementById("box");
-ReactDOM.render(<IndexComponent/>, oBox);
+// var oBox = document.getElementById("box");
+// ReactDOM.render(<IndexComponent/>, oBox);
--- src/main.js
+++ src/main.js
@@ -8,17 +8,20 @@
 // //分开引入
 // import "./bootstrap-demo/bootstrap.include"
 
- //import {IndexComponent} from "./components/index.component";
-
-//export default IndexComponent;
-
 // import "./components/game.component";
 
 // import {test} from './components/test';
 
 // test();
 
+
+// 先引入, 再export default, 然后npm run pub打包成可发布到npm的js
+//import {IndexComponent} from "./components/index.component";
+
+//export default IndexComponent;
+
 import "./components/index.component";
 
+//然后从npm中安装发布的模块, 引入组件
 var app = require ('../dist/static/js/app').default;
 console.log(app);
